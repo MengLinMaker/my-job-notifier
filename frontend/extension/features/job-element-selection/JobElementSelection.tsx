92ac217de21ecf720406d38263ab50c3f10325d6
MengLinMaker <menglinmaker@gmail.com>
2026-05-22 08:29:31 +1000

    feat: job element selection should pass class up

--- frontend/extension/features/job-element-selection/JobElementSelection.tsx
+++ frontend/extension/features/job-element-selection/JobElementSelection.tsx
@@ -10,22 +10,30 @@ import {
 import { EditButton } from './components/EditButton'
 import { JobElementClassText } from './components/JobElementClassText'
 
-export function JobElementSelection(props: { origin: string; tabId: number }) {
-    const [selectedClassName, setSelectedClassName] = useSyncedStorageState<string | null>(
+export function JobElementSelection(props: {
+    onJobElementClassChange: (jobElementClass: string | null) => void
+    origin: string
+    tabId: number
+}) {
+    const [jobElementClass, setJobElementClass] = useSyncedStorageState<string | null>(
         JOB_ELEMENT_CLASS_STORAGE_KEY(props.origin),
         null,
     )
     const [hoveredClassName, setHoveredClassName] = useState<string | null>(null)
     const [isEditing, setIsEditing] = useState(false)
-    const displayedClassName = isEditing ? hoveredClassName : selectedClassName
+    const displayedClassName = isEditing ? hoveredClassName : jobElementClass
 
     useEffect(() => {
-        if (isEditing || !selectedClassName) return
+        props.onJobElementClassChange(jobElementClass)
+    }, [jobElementClass, props.onJobElementClassChange])
+
+    useEffect(() => {
+        if (isEditing || !jobElementClass) return
         browser.tabs.sendMessage(props.tabId, {
             type: SET_JOB_ELEMENT_CLASS_MESSAGE,
-            className: selectedClassName,
+            className: jobElementClass,
         })
-    }, [isEditing, selectedClassName, props.tabId])
+    }, [isEditing, jobElementClass, props.tabId])
 
     useEffect(() => {
         const port = browser.tabs.connect(props.tabId, { name: HOVERED_CLASS_PORT })
@@ -33,17 +41,19 @@ export function JobElementSelection(props: { origin: string; tabId: number }) {
             setIsEditing(message.isEditing)
             if (message.isEditing) return setHoveredClassName(message.className)
             setHoveredClassName(null)
-            setSelectedClassName(message.className)
+            setJobElementClass(message.className)
         })
-        return () => port.disconnect()
-    }, [setSelectedClassName, props.tabId])
+        return () => {
+            port.disconnect()
+        }
+    }, [setJobElementClass, props.tabId])
 
     function trackJobPostings() {
         setHoveredClassName(null)
         setIsEditing(!isEditing)
         browser.tabs.sendMessage(props.tabId, {
             type: isEditing ? CANCEL_JOB_TRACKING_MESSAGE : START_JOB_TRACKING_MESSAGE,
-            className: selectedClassName,
+            className: jobElementClass,
         })
     }
 
@@ -61,7 +71,7 @@ export function JobElementSelection(props: { origin: string; tabId: number }) {
             <JobElementClassText
                 jobElementClass={displayedClassName}
                 isEditing={isEditing}
-                onJobElementClassChange={setSelectedClassName}
+                onJobElementClassChange={setJobElementClass}
             />
         </section>
     )
